89970c833d1785776bb3d4a1550b67847bf43e1e
Bryan Stearns <bryanstearns@gmail.com>
2013-03-27 15:58:01 -0700

    Remove empty action methods - they're not needed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,2 @@
 class HomeController < ApplicationController
-  def render_loop
-  end
-
-  def render_collection
-  end
-
-  def render_content_tag_helper
-  end
-
-  def render_raw_html_helper
-  end
-
-  def single_view
-  end
 end
