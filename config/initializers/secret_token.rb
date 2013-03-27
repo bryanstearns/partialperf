4cc0d391d3ebe13550a4606668c9108705958ea1
Bryan Stearns <bryanstearns@gmail.com>
2013-03-27 09:07:09 -0700

    rails new partialperf -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partialperf::Application.config.secret_token = 'bf62bba5164a957b432d36f2fb3d5a21e30736bd6dbb3c014d5b84229d054e523a1321d8438a5349bdd7ee1a9c71849700183c404b8160ae2f6e473426f99c8f'
