813cc2e1e18f6a005853c27d97a94827bc999ce3
Bryan Stearns <bryanstearns@gmail.com>
2013-03-27 16:46:27 -0700

    Load samples, if any, for graphing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,18 @@ class HomeController < ApplicationController
   MEASUREMENT_ACTIONS = Dir.glob("app/views/home/*.html.erb")\
                            .map {|f| File.basename(f).split('.').first }\
                            - %w[_i index]
+
+  def index
+    @samples = read_samples
+  end
+
+protected
+  def read_samples
+    Dir.glob("log/#{Rails.env}.*.samples").inject({}) do |h, sample_file|
+      action = File.basename(sample_file).split(".")[1]
+      samples = File.readlines(sample_file).map(&:to_i)
+      h[action] = samples
+      h
+    end
+  end
 end
